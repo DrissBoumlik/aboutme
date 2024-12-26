0941faaebe608fe264b23c7ded13ba39d84bc07f
Driss <drissboumlik.dev@gmail.com>
2024-12-26 12:04:21 +0100

    Edit content

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import { initParticlesJS, initDarkMode, getMyAge, animatedWelcome } from "./scripts/functions";
+import { initParticlesJS, initDarkMode, animatedWelcome } from "./scripts/functions";
 import 'lazysizes';
 
 initDarkMode('.toggle-dark-mode')
 initParticlesJS()
 animatedWelcome()
-document.getElementById('myage').innerHTML = getMyAge();
